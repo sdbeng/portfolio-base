b56a1cfd12b771c25dcaa9f647bba122871eb41c
Sergio D. Barrera <sdbeng@gmail.com>
2024-09-20 23:20:46 -0700

    got data from supa db

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,14 +1,34 @@
-import { getResumeData } from "@/utils/fetchdb"
+import {createClient} from '@supabase/supabase-js'
+
+const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 export default async function PortfolioHome() {
-    
-    const resumeData = await getResumeData()
+    // console.log('LOG supabase instance===', supabase)
+    try {
+        const {data, error, status} = await supabase
+            .from('resume')
+            .select(`name, title, summary`)
+            
 
-    if(!resumeData) {
-        return <div>No resume data found.</div>
+        if(error && status !== 406) {
+            console.error(error)
+            throw error
+        }
+        
+        if(!data) {
+            return <div>No resume data found.</div>
+        }
+        console.log('status===', status)
+        console.log('data===', data)
+        return <div>{JSON.stringify(data)}</div>;
+    } catch (error) {
+        console.error('Error fetching resume data', error)
+        
     }
 
-    console.log('resumeData===', resumeData)
+
 
     return(
         <>
